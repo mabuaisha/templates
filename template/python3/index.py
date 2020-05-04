73c57fbfeb3db7383d3ab94454ab06bd31f915b5
mabuaisha <mabuaisha@outlook.com>
2020-05-04 12:37:15 +0300

    update python http

--- template/python3/index.py
+++ template/python3/index.py
@@ -1,21 +1,69 @@
-# Copyright (c) Alex Ellis 2017. All rights reserved.
-# Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#!/usr/bin/env python
+from flask import Flask, request, jsonify
+from waitress import serve
+import os
 
-import sys
 from function import handler
 
-def get_stdin():
-    buf = ""
-    while(True):
-        line = sys.stdin.readline()
-        buf += line
-        if line == "":
-            break
-    return buf
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    st = get_stdin()
-    ret = handler.handle(st)
-    if ret != None:
-        print(ret)
+class Event:
+    def __init__(self):
+        self.body = request.get_data()
+        self.headers = request.headers
+        self.method = request.method
+        self.query = request.args
+        self.path = request.path
+
+class Context:
+    def __init__(self):
+        self.hostname = os.getenv('HOSTNAME', 'localhost')
+
+def format_status_code(resp):
+    if 'statusCode' in resp:
+        return resp['statusCode']
+    
+    return 200
+
+def format_body(resp):
+    if 'body' not in resp:
+        return ""
+    elif type(resp['body']) == dict:
+        return jsonify(resp['body'])
+    else:
+        return str(resp['body'])
+
+def format_headers(resp):
+    if 'headers' not in resp:
+        return []
+    elif type(resp['headers']) == dict:
+        headers = []
+        for key in resp['headers'].keys():
+            header_tuple = (key, resp['headers'][key])
+            headers.append(header_tuple)
+        return headers
+    
+    return resp['headers']
+
+def format_response(resp):
+    if resp == None:
+        return ('', 200)
+
+    statusCode = format_status_code(resp)
+    body = format_body(resp)
+    headers = format_headers(resp)
+
+    return (body, statusCode, headers)
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
+def call_handler(path):
+    event = Event()
+    context = Context()
+    response_data = handler.handle(event, context)
+    
+    resp = format_response(response_data)
+    return resp
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=5000)
